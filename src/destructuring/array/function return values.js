4dc830126a2ecc51e6f2e56d4f737b292c47a102
Abhishek <abhishek.konnayil@gmail.com>
2025-04-07 22:54:45 +0530

    function return values

---
+++ src/destructuring/array/function return values.js
@@ -0,0 +1,11 @@
+function getCoordinates() {
+    return [12.35, 31.25]
+}
+const [latitude, longitude] = getCoordinates();
+console.log(latitude)
+console.log(longitude)
+
+const marks = () => [35, 45]
+const [maths, physics] = marks();
+console.log(maths)
+console.log(physics)
