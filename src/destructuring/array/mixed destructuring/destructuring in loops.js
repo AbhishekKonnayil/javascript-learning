7e06c8d294716042772658d9e8d9f289c01589a4
Abhishek <abhishek.konnayil@gmail.com>
2025-04-08 10:57:00 +0530

    destructuring in loops

---
+++ src/destructuring/array/mixed destructuring/destructuring in loops.js
@@ -0,0 +1,4 @@
+const users = [{ name: "Abhishek", id: 1, email: "abhishek.konnayil@gmail.com" }, { name: "Abhijith", id: 2, email: "abhijithk2012@gmail.com" }]
+for (const { email } of users) {
+    console.log(email);
+} 
